2517e57d2889f805f5dd35bb476ff23c5931a37b
username <youremail@domain.com>
2023-02-03 12:56:50 -0800

    updated css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import NavBar from "./NavBar/NavBar";
+import AboutMe from "./AboutMe/AboutMe";
+import MyProjects from "./MyProjects/MyProjects";
+export default function app() {
+  // const project = [{Vacation Tracker}]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <AboutMe />
+      <MyProjects />
     </div>
   );
 }
-
-export default App;
---
+++ src/MyProjects/MyProjects.jsx
@@ -0,0 +1,112 @@
+import "./MyProjects.css";
+export default function MyProjects() {
+  return (
+    <div className="bdy">
+      <h1 className="project">Projects</h1>
+      <div className="cards">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            // className="c4"
+            src="https://i.imgur.com/AWAxMoE.png"
+            title="source: imgur.com"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Connect Four</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            // className="c4"
+            src="https://i.imgur.com/AWAxMoE.png"
+            title="source: imgur.com"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Vacation Tracker</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            // className="c4"
+            src="https://i.imgur.com/AWAxMoE.png"
+            title="source: imgur.com"
+          />
+          <div className="card-body">
+            <h5 className="card-title">Bidding Wars</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card" style={{ width: "18rem" }}>
+          {/* <img
+            // className="c4"
+            src="https://i.imgur.com/AWAxMoE.png"
+            title="source: imgur.com"
+          /> */}
+          <div className="card-body">
+            <h5 className="card-title">Hiking California</h5>
+            <img
+              src="https://i.imgur.com/pSQxHyP.png"
+              title="source: imgur.com"
+            />
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+
+        {/* <div>
+        <img
+          className="c4"
+          src="https://i.imgur.com/AWAxMoE.png"
+          title="source: imgur.com"
+        />
+
+        <a href="https://ahaines10.github.io/connect-four/">Connect Four</a>
+      </div> */}
+        {/* <div>
+          <img
+            className="v4"
+            src="https://i.imgur.com/tlJ8FB5.png"
+            title="source: imgur.com"
+          />
+
+          <a href="https://vacation-tracker14.herokuapp.com/">
+            Vacation Tracker
+          </a>
+        </div>
+        <div>
+          <img
+            className="b4"
+            src="https://i.imgur.com/nWTvQ3G.png"
+            title="source: imgur.com"
+          />
+
+          <a href="https://biddingwars.herokuapp.com">Bidding Wars</a>
+        </div> */}
+      </div>
+    </div>
+  );
+}
